6bc61c415731fb98ccbce3f1de4fc0aa41eacd08
javier <javierkebin@gmail.com>
2023-03-09 23:59:36 -0500

    Subiendo mi portafolio

---
+++ scripts/script.js
@@ -0,0 +1,16 @@
+const inputs = document.querySelectorAll('input');
+inputs.forEach(
+    input => {
+        input.onfocus = function () {
+            input.previousElementSibling.classList.add('focus');
+            input.classList.add('bottom')
+        }
+        input.onblur = function () {
+            input.value = input.value.trim();
+            if (input.value.trim().length == 0) {
+                input.previousElementSibling.classList.remove('focus');
+            }
+            input.classList.remove('bottom')
+        }
+    }
+);
