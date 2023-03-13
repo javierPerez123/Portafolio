e6a9ff0dda51cfd378dc01d64d07da74c67b1bf8
javierPerez123 <122051888+javierPerez123@users.noreply.github.com>
2023-03-12 23:01:25 -0500

    fin de validadcion de formulario

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,13 @@ import { scriptMenu } from './scriptsMenu.js'
 
 scriptMenu()
 
+const pError = (texto) => {
+    const elemento = document.createElement('p');
+    elemento.classList.add('pError0');
+    elemento.innerHTML = texto
+    return elemento
+}
+
 const inputs = document.querySelectorAll('input[class^="formulario__entrada"]');
 inputs.forEach(
     input => {
@@ -13,21 +20,43 @@ inputs.forEach(
         }
         input.onblur = function () {
             input.value = input.value.trim();
-            console.log(input.value.trim())
-            console.log(input.value)
+            const id = input.getAttribute('id')
+            let nombreInput;
+            if (id == "name") {
+                nombreInput = "Nombres"
+            }
+            if (id == "email") {
+                nombreInput = "Email"
+            }
+            if (id == "asunto") {
+                nombreInput = "Asunto"
+            }
+            if (id == "mensaje") {
+                nombreInput = "Mensaje"
+            }
             if (input.value.trim().length == 0) {
                 input.previousElementSibling.classList.remove('focus');
                 input.classList.add('error1')
+
+                if (input.parentElement.parentElement.querySelector('p') == null) {
+                    input.parentElement.parentElement.appendChild(pError(`Campo ${nombreInput} no debe estar en blanco o vacío.`))
+                }
             }
             else {
                 input.classList.remove('error1')
+                if (input.parentElement.parentElement.querySelector('p') != null) {
+                    const divP = input.parentElement.parentElement.querySelector('p')
+                    divP.remove()
+                }
+
             }
+
             if (input.value.trim().length == 1 || input.value.trim().length == 2) {
                 input.classList.add('error1')
+                if (input.parentElement.parentElement.querySelector('p') == null) {
+                    input.parentElement.parentElement.appendChild(pError(`Campo ${nombreInput} debe ser valido.`))
+                }
             }
-
-
-
 
             input.classList.remove('bottom')
 
